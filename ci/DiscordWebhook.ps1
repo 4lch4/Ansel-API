71f85506b6ae3cda55a583714b8432bba8442128
Alcha <dleaman212@gmail.com>
2019-10-27 16:15:43 -0500

    Added ci folder and build scripts

---
+++ ci/DiscordWebhook.ps1
@@ -0,0 +1,28 @@
+$WHURL = #(DISCORD_WH_URL)#
+
+$ReqBody = @{
+  content = "$(Build.QueuedBy) queued a build for the $(Build.Repository.Name) repository on the ``$(Build.SourceBranchName)`` branch.";
+  embeds = @(
+    @{
+      title = '$(Build.DefinitionName)';
+      description = "Build on ``$(Build.SourceBranchName)`` branch has completed.";
+      url = 'https://dev.azure.com/4lch4/Ansel/_build/results?buildId=$(Build.BuildId)';
+      fields = @(
+        @{
+          name = 'Job Status';
+          value = '$(Agent.JobStatus)';
+        },
+        @{
+          name = 'Build Id';
+          value = '$(Build.BuildId)';
+        }
+      )
+    }
+  )
+}
+
+$Json = $ReqBody | ConvertTo-Json -Depth 10 -Compress
+
+Write-Host $Json
+
+Invoke-RestMethod -Uri $WHURL -Method Post -ContentType 'application/json' -Body $Json
---
+++ ci/restartContainer.sh
@@ -0,0 +1,11 @@
+# Stop currently running container.
+docker stop ansel
+
+# Deleting existing container.
+docker rm ansel
+
+# Pull latest version of image.
+docker pull 4lch4/ansel:latest
+
+# Start up container.
+docker run -d -p 4242:4242 4lch4/ansel:latest --name=ansel

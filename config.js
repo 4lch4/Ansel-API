58e658e1e3af0045d5ca76b87a390d7e52a3f13a
Alcha <dleaman212@gmail.com>
2019-10-27 15:25:28 -0500

    Updated config file

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const pkg = require('./package.json')
 
 module.exports = {
-  name: process.env.APP_NAME ? process.env.APP_NAME : pkg.displayName,
-  version: process.env.APP_VERSION ? process.env.APP_VERSION : pkg.version,
-  port: process.env.APP_PORT ? process.env.APP_PORT : 4242
+  name: pkg.displayName,
+  version: pkg.version ? pkg.version : '0.0.0',
+  port: 4242
 }

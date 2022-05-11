3cfc35c1c400c5771d846b19048904101ff1b1cd
4lch4 <git@4lch4.email>
2022-05-11 12:38:08 -0500

    feat: added start of delete & upload tests

--- tests/delete.test.ts
+++ tests/delete.test.ts
@@ -2,3 +2,23 @@
  * DELETE - /delete/:folderName
  * DELETE - /delete/:folderName/:assetId
  */
+const baseUrl = 'http://localhost:8080/api/v1'
+
+describe('Test /delete/:folderName/:assetId route', () => {
+  test('Can delete a specific asset within a folder', async () => {
+    const delRes = await axios.delete(`${baseUrl}/delete/test/0`)
+    console.log(delRes)
+
+    expect(delRes.status).toBe(200)
+  })
+})
+
+describe('Test /delete/:folderName route', () => {
+  test('Can delete an entire asset folder', async () => {
+    const delRes = await axios.delete(`${baseUrl}/delete/test`)
+
+    console.log(delRes)
+
+    expect(delRes.status).toBe(200)
+  })
+})
